a9077be5ea4e3b2ca5e6a17adb1c3efdb5fc2515
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:39:20 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// markenanmeldung-dpma - DPMA Markenanmeldung - Deutsche Markenregistrierung automatisiert. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
